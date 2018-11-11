3e77bfbd8e37931c2c929ffbeef1d2f836d77a1a
Debvelop3A <Debvelop3A@github.com>
2018-11-11 23:24:50 +0900

    Player & Bullet Scripts Modify
    Ang Gimotti

--- 2d fortress/Assets/02.Script/BulletScript.cs
+++ 2d fortress/Assets/02.Script/BulletScript.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    Player playerScript;
-    float speed = 300;
-
-    Vector3 velocity = new Vector3(300.0f, 300.0f, 0.0f);
+    public float force = 300;
 
 	void Start ()
     {
-        playerScript = FindObjectOfType<Player>();
-        GetComponent<Rigidbody2D>().AddRelativeForce(transform.right * speed);
-        //GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
-        //GetComponent<Rigidbody2D>().velocity = new Vector3(5, 5, 0);
-        //GetComponent<Rigidbody2D>().AddForce(velocity);
-        //GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.right * force);
     }
-
-    void Update ()
-    {
-		
-	}
 }
--- 2d fortress/Assets/02.Script/Player.cs
+++ 2d fortress/Assets/02.Script/Player.cs
@@ -12,27 +12,21 @@ public class Player : MonoBehaviour
     Animator anim;
 
     bool shot = false;
-    bool isShoot = false;
+    bool shot_per = false; //shot키를 누를 수 있는 떄를 설정체크
 
 
     void Start ()
     {
-        shot = false;
         anim = transform.GetComponent<Animator>();
         StartCoroutine(Move(1,0));
     }
 
     private void Update()
     {
-        //Debug.Log("isShoot" + isShoot);
-        //Debug.Log("shot" + shot);
-        if (Input.GetMouseButtonDown(0) && isShoot == false)
+        if (Input.GetMouseButtonDown(0) && !shot && shot_per)
         {
             shot = true;
-        }
-        else
-        {
-            isShoot = false;
+            shot_per = false;
         }
     }
 
@@ -47,7 +41,7 @@ public class Player : MonoBehaviour
             if (index == 0)
             {
                 anim.SetInteger("StateType", 0);
-                Fuel = 1000;
+                Fuel = 10;
                 Debug.Log("0번 상태");
                 while (Fuel >= 0)
                 {
@@ -68,21 +62,23 @@ public class Player : MonoBehaviour
             {
                 anim.SetInteger("StateType", 1);
                 posin.SetActive(true);
+                shot_per = true;
+                Vector3 Angle = new Vector3(0, 0, 0);
                 
                 Debug.Log("1번 상태");
-                while (isShoot == false)
+                while (!shot)
                 {
                     float angle = posin.transform.eulerAngles.z+Input.GetAxis("Vertical");                   
                     Vector3 Rot = new Vector3(0, 0, Mathf.Clamp(angle, 0, 80));
                     posin.transform.eulerAngles = Rot;
+                    Angle = Rot;
                    
-                    if (shot)
-                    {
-                        GameObject Bullet = Instantiate(bullet, firePos.position, Quaternion.Euler(Rot));
-                        isShoot = true;
-                        shot = false;
-                    }
                     yield return null;
+                }
+                if (shot)
+                {
+                    GameObject Bullet = Instantiate(bullet, firePos.position, Quaternion.Euler(Angle));
+                    Debug.Log(Angle);
                 }
                 index = 2;
                 posin.SetActive(false);
@@ -99,83 +95,13 @@ public class Player : MonoBehaviour
                 Debug.Log("0번으로 돌아가기 위한 대기시간");
                 yield return new WaitForSeconds(2f);
 
+                shot = false;
+                shot_per = false;
             }
         }
         
 
     }
-
-    //[Header(" - 포물선 운동 정보 - ")]
-    //[SerializeField] private float fMaxHeight;
-    //[SerializeField] private float fMaxTime;
-    //public GameObject Bullet;
-    //public Transform Boob;
-    //public Vector3 m_tagetPos;
-    //public Transform FirePos;
-    //public Transform TargetPos;
-
-
-    //Transform m_objTr;
-
-    //Vector3 m_startPos; // 출발지
-    //Vector3 m_targetPos; // 도착지
-    //Vector3 m_currentPos; // 현재 위치
-
-    //float m_fSpeedX; // X축 스피드
-    //float m_fSpeedY; // Y축 스피드
-    //float m_fSpeedZ; // Z축 스피드
-
-    //float m_fg; // Y축으로의 중력가속도
-    //float m_fEndTime; // 도착 지점 도달 시간
-    //float m_fMaxHeight; // 최대 높이
-    //float m_fHeight; // 최대 높이 - 시작 높이
-    //float m_fTargetHeight; // 도착지점 높이 - 시작지점 높이 
-    //float m_fTime = 0f; // 흐르는 시간
-    //float m_fMaxTime; // 최대 높이까지 가는 시간
-
-    //IEnumerator Shot(Transform _objTr, Vector3 _startPos, Vector3 _targetPos, float _fMaxHeight, float _fMaxTime)
-    //{
-        
-    //       m_objTr = _objTr;
-       
-    //       m_startPos = _startPos;
-    //       m_targetPos = _targetPos;
-    //       m_fMaxHeight = _fMaxHeight;
-    //       m_fMaxTime = _fMaxTime;
-       
-    //       m_fTargetHeight = m_targetPos.y - m_startPos.y;
-    //       m_fHeight = m_fMaxHeight - m_startPos.y;
-    //       m_fg = 2 * m_fHeight / (m_fMaxTime * m_fMaxTime);
-    //       m_fSpeedY = Mathf.Sqrt(2 * m_fg * m_fHeight);
-       
-    //       float a = m_fg;
-    //       float b = -2 * m_fSpeedY;
-    //       float c = 2 * m_fTargetHeight;
-       
-    //       m_fEndTime = (-b + Mathf.Sqrt(b * b - 4 * a * c)) / (2 * a);
-       
-    //       m_fSpeedX = -(m_startPos.x - m_targetPos.x) / m_fEndTime;
-    //       m_fSpeedZ = -(m_startPos.z - m_targetPos.z) / m_fEndTime;
-       
-    //       while (true)
-    //       {
-    //           m_fTime += Time.deltaTime;
-       
-    //           m_currentPos.x = m_startPos.x + m_fSpeedX * m_fTime;
-    //           m_currentPos.y = m_startPos.y + (m_fSpeedY * m_fTime) - (0.5f * m_fg * m_fTime * m_fTime);
-    //           m_currentPos.z = m_startPos.z + m_fSpeedZ * m_fTime;
-       
-    //           m_objTr.LookAt(m_currentPos);
-       
-    //           m_objTr.position = m_currentPos;
-       
-    //           if (m_fTime > m_fEndTime)
-    //           {
-    //               break;
-    //           }
-       
-    //           yield return null;
-    //       }
-    //}
+    
 }
 
