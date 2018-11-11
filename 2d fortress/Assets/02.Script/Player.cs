bdb1e101ea8d03b1bc8ced5cc9846bfea2bf181e
DevTenSei <43212335+DevTenSei@users.noreply.github.com>
2018-11-12 04:32:13 +0900

    TankPosin Modify

--- 2d fortress/Assets/02.Script/Player.cs
+++ 2d fortress/Assets/02.Script/Player.cs
@@ -8,24 +8,28 @@ public class Player : MonoBehaviour
     [Header("Tank's Status")]
     public float Bullet_Force = 300.0f;
     public float Explosion_Radius = 0.0f;//포탄의 폭발범위
-    //여기에 이동속도나 그런것들도 추가시켜주면 감사하겠슴. 
+    public int Fuel = 1000;
+    public int speed = 1;
 
-        [Space(20)]
+
+    [Space(20)]
     [Header("Requirements")]
     public GameObject posin;
     public GameObject bullet;
     public Transform firePos;
+    
 
     Animator anim;
+    SpriteRenderer spriteRenderer;
 
     bool shot = false;
     bool shot_per = false; //shot키를 누를 수 있는 떄를 설정체크
 
-
     void Start ()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         anim = transform.GetComponent<Animator>();
-        StartCoroutine(Move(1,0));
+        StartCoroutine(Move(0));
         if (Explosion_Radius == 0.0f) Explosion_Radius = 0.5f; //조작안할시에는 자동으로 0.5가됨
     }
 
@@ -38,18 +42,14 @@ public class Player : MonoBehaviour
         }
     }
 
-    IEnumerator Move(int turn, int index)
+    IEnumerator Move(int index)
     {
-        int Fuel = 1000;
-        int speed = 1;
-        
-
-        while (turn == 1)
-        {         
+        while(true)
+        {
             if (index == 0)
             {
                 anim.SetInteger("StateType", 0);
-                Fuel = 10;
+                Fuel = 1000;
                 Debug.Log("0번 상태");
                 while (Fuel >= 0)
                 {
@@ -72,15 +72,23 @@ public class Player : MonoBehaviour
                 posin.SetActive(true);
                 shot_per = true;
                 Vector3 Angle = new Vector3(0, 0, 0);
-                
+
                 Debug.Log("1번 상태");
                 while (!shot)
                 {
-                    float angle = posin.transform.eulerAngles.z+Input.GetAxis("Vertical");                   
-                    Vector3 Rot = new Vector3(0, 0, Mathf.Clamp(angle, 0, 80));
+                    float angle = posin.transform.eulerAngles.z + Input.GetAxis("Vertical");
+                    Vector3 Rot = new Vector3(0, 0, angle);
                     posin.transform.eulerAngles = Rot;
                     Angle = Rot;
-                   
+                    //Debug.Log("angle : " + angle);
+                    if ((angle > 90 && angle < 270))
+                    {
+                        spriteRenderer.flipX = true;
+                    }
+                    if ((angle > 270 || angle < 90))
+                    {
+                        spriteRenderer.flipX = false;
+                    }
                     yield return null;
                 }
                 if (shot)
@@ -97,7 +105,7 @@ public class Player : MonoBehaviour
                 posin.transform.localRotation = Quaternion.identity;
                 Debug.Log("2번 상태");
                 index = 0;
-                turn = 1;
+                //turn = 1;
 
                 Debug.Log("0번으로 돌아가기 위한 대기시간");
                 yield return new WaitForSeconds(2f);
@@ -106,8 +114,6 @@ public class Player : MonoBehaviour
                 shot_per = false;
             }
         }
-        
-
     }
     
     public void Bullet_Gen(Vector3 pos, Quaternion Angle)
@@ -115,6 +121,12 @@ public class Player : MonoBehaviour
         GameObject Bullet = Instantiate(bullet, pos, Angle);
         Bullet.GetComponent<BulletScript>().ex_radius = Explosion_Radius;
         Bullet.GetComponent<BulletScript>().force = Bullet_Force;
+    }
+
+    public void SkipIndex()
+    {
+
+
     }
 }
 
